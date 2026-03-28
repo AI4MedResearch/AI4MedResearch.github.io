80a59b6ad5ff77d49fdb3ef8e7f259c53fd033ff
ghif <mghifary@gmail.com>
2026-03-28 04:51:29 +0000

    Deploying to gh-pages from @ AI4MedResearch/AI4MedResearch.github.io@6bf56d6aaedb19c55efc7a6d416649a71a610292 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AI4MedResearch/blog",
+    "data-repo": "AI4MedResearch/AI4MedResearch.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
